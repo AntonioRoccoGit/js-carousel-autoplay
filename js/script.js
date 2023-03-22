6b03d233abc52535afa28cea01ffcdac187dbc18
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-22 10:13:57 +0100

    refactor code

--- js/script.js
+++ js/script.js
@@ -7,63 +7,45 @@ let thumbNailsItems = document.querySelector(".thumbnails-image");
 const nextUp = document.querySelector(".next-up");// bottone next
 const prevDown = document.querySelector(".prev-down");//bottone prev
 
-//grid img and thumbnail generator
-for (let i = 0; i < userImages.length; i++) {
-    
-    let currentImg = userImages[i];
-    
-    sliderItemsContainer.innerHTML +=  
-    `<div class="item">
-    <img src="${currentImg}" alt="">
-    </div>`;
-    thumbNailsItems.innerHTML +=  
-    `<div class="thumb-item">
-        <img src="${currentImg}" alt="">
-    </div>`;
-};
+//genero le grid dell'immagini
+imgInAGrid(userImages);
 
-//setto stato iniziale ultimo bonus
-const thumbNailsItemsArrey = document.querySelectorAll(".thumb-item");
-console.log(thumbNailsItemsArrey);
-//setto lo stato iniziale dove appare almeno la prima immagine
-const sliderItemsArray = document.querySelectorAll(".item");
-
+//setto stato iniziale della pagina
 let itemCounter = 0;
-
+const thumbNailsItemsArrey = document.querySelectorAll(".thumb-item");
+const sliderItemsArray = document.querySelectorAll(".item");
 sliderItemsArray[itemCounter].classList.add("active");
 thumbNailsItemsArrey[itemCounter].classList.add("active");
 
-myAutoInterval = setInterval(goPreviousl, 3000);
-
 //impostiamo il bottone next
-nextUp.addEventListener("click", function(){
+nextUp.addEventListener("click", function () {
     goNext();
     clearInterval(myAutoInterval);
     myAutoInterval = setInterval(goPreviousl, 3000);
 });
 
 //imposto il bottone prev
-prevDown.addEventListener("click", function(){
+prevDown.addEventListener("click", function () {
     //do al bottone la possibilita di tornrare indietro
     goPreviousl();
     //blocco il vecchio intervallo
     clearInterval(myAutoInterval);
     myAutoInterval = setInterval(goPreviousl, 3000);
 
-});    
-
-sliderItemsContainer.addEventListener("mouseenter", function(){
-    clearInterval(myAutoInterval);
 });
-sliderItemsContainer.addEventListener("mouseout", function(){
-    myAutoInterval = setInterval(goPreviousl, 3000);
-});
-
-
-
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////AUTO PLAY
+
+myAutoInterval = setInterval(goPreviousl, 3000);
+
+//aggiungiamo la possibilita di avere un clear on hover sullo slider
+sliderItemsContainer.addEventListener("mouseenter", function () {
+    clearInterval(myAutoInterval);
+});
+sliderItemsContainer.addEventListener("mouseout", function () {
+    myAutoInterval = setInterval(goPreviousl, 3000);
+});
 
 //setto stato iniziale del mio intervallo
 // let myAutoInterval = false;
@@ -78,16 +60,30 @@ sliderItemsContainer.addEventListener("mouseout", function(){
 
 // }
 
-//aggiungiamo la possibilita di avere un clear on hover sullo slider
-
-//gestiamo lo scorrimento a tempo delle slide
-//itemCounter è la variabile che mi permette di scorrere tra le immagini
-
-
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////MY FUNCTION
+
+function imgInAGrid(myImgArray) {
+
+    //grid img and thumbnail generator
+    for (let i = 0; i < myImgArray.length; i++) {
+
+        let currentImg = myImgArray[i];
+
+        sliderItemsContainer.innerHTML +=
+            `<div class="item">
+            <img src="${currentImg}" alt="">
+            </div>`;
+        thumbNailsItems.innerHTML +=
+            `<div class="thumb-item">
+            <img src="${currentImg}" alt="">
+            </div>`;
+    };
+}
+
+
 function goPreviousl() {
-    if (itemCounter === 0){
+    if (itemCounter === 0) {
 
         sliderItemsArray[itemCounter].classList.remove("active");
         thumbNailsItemsArrey[itemCounter].classList.remove("active");
@@ -97,11 +93,11 @@ function goPreviousl() {
         sliderItemsArray[itemCounter].classList.add("active");
         thumbNailsItemsArrey[itemCounter].classList.add("active");
 
-    }else {
+    } else {
 
         sliderItemsArray[itemCounter].classList.remove("active");
         thumbNailsItemsArrey[itemCounter].classList.remove("active");
-        
+
         //aumento il contantore
         itemCounter--;
 
@@ -112,16 +108,16 @@ function goPreviousl() {
 }
 
 function goNext() {
-    if (itemCounter === sliderItemsArray.length - 1){
+    if (itemCounter === sliderItemsArray.length - 1) {
 
         sliderItemsArray[itemCounter].classList.remove("active");
         thumbNailsItemsArrey[itemCounter].classList.remove("active");
 
-        itemCounter = 0; 
+        itemCounter = 0;
 
         sliderItemsArray[itemCounter].classList.add("active");
         thumbNailsItemsArrey[itemCounter].classList.add("active");
-    }else {
+    } else {
 
         //rimuovo classe precedente
         sliderItemsArray[itemCounter].classList.remove("active");
